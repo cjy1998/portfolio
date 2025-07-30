3faa4282de80603c3b8a2d2b68fba7ae1ca90980
cjy <1404340013@qq.com>
2025-07-30 11:03:47 +0800

    fix:暂存

--- components/QrCodeDialog.tsx
+++ components/QrCodeDialog.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import domtoimage from "dom-to-image";
+
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "./ui/dialog";
 import ShareCard from "./ShareCard";
+import { Button } from "./ui/button";
 
 const QrCodeDialog = ({
   open,
@@ -15,26 +19,34 @@ const QrCodeDialog = ({
   open: boolean;
   onOpenChange: (open: boolean) => void;
 }) => {
+  const shareCardRef = useRef<HTMLDivElement>(null);
+  const download = () => {
+    domtoimage.toJpeg(shareCardRef.current as HTMLElement).then((dataUrl) => {
+      const link = document.createElement("a");
+      link.href = dataUrl;
+      link.download = "share-card.jpeg";
+      link.click();
+    });
+  };
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <form>
         {/* <DialogTrigger asChild>
           <Button variant="outline">Open Dialog</Button>
         </DialogTrigger> */}
-        <DialogContent className="sm:max-w-[425px]">
+        <DialogContent className="sm:max-w-[525px]">
           <DialogHeader>
             <DialogTitle>分享卡片</DialogTitle>
             {/* <DialogDescription>创建个人资料卡片，一键分享</DialogDescription> */}
           </DialogHeader>
           <div className="w-full flex justify-center">
-            <ShareCard />
+            <ShareCard ref={shareCardRef} />
           </div>
-          {/* <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter> */}
+          <DialogFooter>
+            <Button onClick={download} type="submit">
+              保存
+            </Button>
+          </DialogFooter>
         </DialogContent>
       </form>
     </Dialog>
--- components/ShareCard.tsx
+++ components/ShareCard.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import Image from "next/image";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { initData } from "@/lib/utils";
-import { Mail, Github, QrCode } from "lucide-react";
+import { Mail, Github } from "lucide-react";
+import { QRCodeCanvas } from "qrcode.react";
+import { forwardRef } from "react";
+
 const images = [
   "/svg/js.svg",
   "/svg/tailwindcss.svg",
@@ -13,60 +16,66 @@ const images = [
   "/svg/nextjs.svg",
   "/svg/nestjs.svg",
 ];
-const ShareCard = () => {
+const ShareCard = forwardRef<HTMLDivElement>((props, ref) => {
+  // bg-white/30 backdrop-blur-lg border-white/40
   return (
-    <div className="flex flex-col items-center  gap-2 w-[300px] p-6 bg-white/30 backdrop-blur-lg rounded-2xl border border-white/40 shadow-lg">
+    <div
+      ref={ref}
+      className="flex flex-col items-center  bg-white gap-2 w-[500px] p-6  rounded-2xl border  shadow-lg"
+    >
       {/* 顶部头像 和 用户名 座右铭 */}
       <div className="flex items-center gap-3">
-        <Avatar className="w-[72px] h-[72px]">
-          <AvatarImage src="/avator.jpg" />
-        </Avatar>
         <div className="flex flex-col gap-2">
+          <Avatar className="w-[96px] h-[96px]">
+            <AvatarImage src="/avator.jpg" />
+          </Avatar>
           <div className="flex gap-2">
             {initData.username && (
-              <p className="text-[16px] font-bold">{initData.username}</p>
+              <p className="text-[24px] font-bold">{initData.username}</p>
             )}
             {initData.address && (
-              <p className="text-[10px] text-gray-600 self-end">
+              <p className="text-[14px] text-gray-600 self-end">
                 {initData.address}
               </p>
             )}
           </div>
 
           {initData.motto && (
-            <em className="text-[12px] text-gray-600 ">{initData.motto}</em>
+            <em className="text-[16px] text-gray-600 ">{initData.motto}</em>
           )}
         </div>
-      </div>
-      {/* 社交信息 */}
-      <div className="flex flex-col  gap-3 self-start px-2 py-1">
-        {initData.email && (
-          <span className="flex gap-1 text-gray-600 text-[14px]">
-            <Mail />
-            {initData.email}
-          </span>
-        )}
-        {initData.github && (
-          <span className="flex gap-1  text-gray-600 text-[14px]">
-            <Github />
-            {initData.github}
-          </span>
-        )}
-      </div>
-      {/* 技术栈 */}
-      <div className="flex gap-2 flex-wrap self-start px-2 py-1">
-        {images.map((src, index) => (
-          <div key={index} className="flex md:justify-start justify-center">
-            <Image src={src} height={18} width={18} alt="技术栈" />
+        {/* 社交信息 */}
+        <div className="flex flex-col  gap-3  py-1 text-gray-600 text-[16px]">
+          {initData.email && (
+            <span className="flex gap-1 ">
+              <Mail />
+              {initData.email}
+            </span>
+          )}
+          {initData.github && (
+            <span className="flex gap-1  ">
+              <Github />
+              {initData.github}
+            </span>
+          )}
+          {/* 技术栈 */}
+          <div className="flex gap-2 flex-wrap  py-1">
+            {images.map((src, index) => (
+              <div key={index} className="flex md:justify-start justify-center">
+                <Image src={src} height={24} width={24} alt="技术栈" />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      {/* 二维码 */}
-      <div className="self-end">
-        <QrCode />
+        </div>
+
+        {/* 二维码 */}
+        <div className="self-end">
+          <QRCodeCanvas value={"https://www.baidu.com/"} size={48} />
+          {/* <QrCode /> */}
+        </div>
       </div>
     </div>
   );
-};
-
+});
+ShareCard.displayName = "ShareCard";
 export default ShareCard;
